b112fd7e3094258b260a79efb964dcc201f75b2c
unknown <nimalan1108@gmail.com>
2021-08-30 21:07:06 +0530

    Display only created profile

--- src/pages/Developers.js
+++ src/pages/Developers.js
@@ -10,6 +10,7 @@ function Developers({ whosProfile }) {
   const [ProfileCreated, isProfileCreated] = useState();
   const [collectionList, setCollection] = useState([]);
 
+  console.log(collectionList);
   useEffect(() => {
     const fetchFromDB = async () => {
       await db
@@ -70,41 +71,43 @@ function RenderProfiles({ collectionList, whosProfile }) {
       className="w-full min-h-91 h-full grid grid-cols-3 justify-items-center items-start mr:grid-cols-1"
       id="users"
     >
-      {collectionList.map((item, i) => (
-        <div
-          className="max-w-sm w-full h-48 bg-white border-2 border-secondary-200 flex flex-col rounded m-5 shadow-2xl hover:shadow-2xl"
-          key={i}
-        >
-          <div className="w-full h-3/4 bg-secondary-300 flex flex-row justify-start items-center">
-            <img
-              alt="profileImage"
-              src={item.picture}
-              className="w-20 h-20 mx-5 rounded-full"
-            ></img>
-            <div className="text-secondary-100">
-              <h3 className="font-nunito text-2xl">{item.name}</h3>
-              <h4 className="font-pop text-lg">{item.currentPosition}</h4>
+      {collectionList.map((item, i) =>
+        item.isProfileCreated === true ? (
+          <div
+            className="max-w-sm w-full h-48 bg-white border-2 border-secondary-200 flex flex-col rounded m-5 shadow-2xl hover:shadow-2xl"
+            key={i}
+          >
+            <div className="w-full h-3/4 bg-secondary-300 flex flex-row justify-start items-center">
+              <img
+                alt="profileImage"
+                src={item.picture}
+                className="w-20 h-20 mx-5 rounded-full"
+              ></img>
+              <div className="text-secondary-100">
+                <h3 className="font-nunito text-2xl">{item.name}</h3>
+                <h4 className="font-pop text-lg">{item.currentPosition}</h4>
+              </div>
             </div>
-          </div>
 
-          <div className="w-full h-1/4 flex justify-end items-center">
-            <button
-              className="mx-5 text-md font-pop text-white bg-primary rounded h-7 w-32 transition hover:bg-primary"
-              id={item.username}
-            >
-              <Link
-                onClick={(e) => {
-                  whosProfile(e.target.id);
-                }}
-                to="/profile"
+            <div className="w-full h-1/4 flex justify-end items-center">
+              <button
+                className="mx-5 text-md font-pop text-white bg-primary rounded h-7 w-32 transition hover:bg-primary"
                 id={item.username}
               >
-                View Profile
-              </Link>
-            </button>
+                <Link
+                  onClick={(e) => {
+                    whosProfile(e.target.id);
+                  }}
+                  to="/profile"
+                  id={item.username}
+                >
+                  View Profile
+                </Link>
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ) : null
+      )}
     </div>
   );
 }
